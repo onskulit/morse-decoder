248ddb8b0f098541baee690d1199d08531ee5d25
onskulit <dislaty@mail.com>
2022-01-19 14:33:13 +0300

    task is solved

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let exprArr = expr.split('');
+    
+    // get array for binary expressed letters
+    let binaryArr = [];
+    for (let i = 0; i < exprArr.length; i += 10) {
+        binaryArr.push(exprArr.slice(i, i + 10));
+    }
+
+    // get array for morse expressed letters
+    let morseArr = binaryArr.map((letter) => {
+        
+        // space check
+        if (letter[0] === '*') {
+            return ' ';
+        }
+
+        let letterArr = [];
+        for (let i = 0; i < letter.length; i += 2) {
+            if (letter[i] === '1' && letter[i + 1] === '0') {
+                letterArr.push('.');
+            } else if (letter[i] === '1' && letter[i + 1] === '1') {
+                letterArr.push('-');
+            }
+        }
+        return letterArr.join('');
+    });
+
+    // get sentence
+    let sentence = morseArr.map((letter) => {
+
+        if (letter === ' ') {
+            return ' ';
+        }
+
+        return MORSE_TABLE[letter];
+    }).join('');
+
+    return sentence;
 }
 
 module.exports = {
